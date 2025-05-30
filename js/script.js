eeae20bf4e995e9a811b38db6bb4765f69b5eb79
Ludex85 <maximiliano_capello@hotmail.com>
2025-05-30 02:06:47 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function irASeccion(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+function verFamilia(nombre) {
+  alert("Mostrando detalles de: " + nombre);
+  // Acá podrías redirigir a una página por familia o mostrar un modal
+}
